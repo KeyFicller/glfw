540bbcfbb1304df8c78cbfb4d55b38ef58d652b0
KeyFicller <799872510@qq.com>
2023-06-18 16:13:47 +0800

    change to dll

--- premake5.lua
+++ premake5.lua
@@ -1,12 +1,12 @@
 project "glfw"
-    kind "StaticLib"
+    kind "SharedLib"
     language "C"
     staticruntime "on"
     
     targetdir(g_workspaceDir .. "/bin/" .. g_outputDir .. "/%{prj.name}")
     objdir(g_workspaceDir .. "/bin-int/" .. g_outputDir .. "/%{prj.name}")
 
-    defines {"_CRT_SECURE_NO_WARNINGS", "_GLFW_WIN32"}
+    defines {"_CRT_SECURE_NO_WARNINGS", "_GLFW_WIN32", "_GLFW_BUILD_DLL"}
 
 	files
 	{
@@ -14,6 +14,11 @@ project "glfw"
         "include/GLFW/glfw3native.h",
         "src/**.h",
         "src/**.c"
+    }
+
+    postbuildcommands
+    {
+        ("{COPY} %{cfg.buildtarget.relpath} \"../../bin/" .. g_outputDir .. "/demo/\"")
     }
     
 	filter "system:windows"
